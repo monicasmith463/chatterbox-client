59149cccb0b04f33e75c3a217b7936c63aa161d0
monicasmith463 <monica.smith463@gmail.com>
2018-03-16 17:31:34 -0700

    Second commit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,102 +1,74 @@
-// // YOUR CODE HERE:
-// // var MakeApp = function() {
-// //   this.posts = [];
-// // };
-//
-// class MakeApp {
-//
-//   constructor() {
-//
-//     init () {
-//
-//     }
-//
-//     send () {
-//
-//     }
-//   }
-//
-//
-//
-//
-//
-// MakeApp.prototype.send = function (message) {
-//   $.ajax({
-//     // This is the url you should use to communicate with the parse API server.
-//     url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-//     type: 'POST',
-//     data: JSON.stringify(message),
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log('chatterbox: Message sent');
-//     },
-//     error: function (data) {
-//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//       console.error('chatterbox: Failed to send message', data);
-//     }
-//   })
+// YOUR CODE HERE:
+// var MakeApp = function() {
+//   this.posts = [];
 // };
-//
-// MakeApp.prototype.fetch = function () {
-//   $.ajax({
-//     type: 'GET',
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log('chatterbox: Message fetched');
-//     },
-//     error: function (data) {
-//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//       console.error('chatterbox: Failed to fetch message', data);
-//     }
-//   })
-// };
-//
-// MakeApp.prototype.clearMessages = function () {
-//   $.ajax({
-//     url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-//     type: 'DELETE',
-//     data: { _method: 'delete' },
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log('chatterbox: Messages cleared');
-//     },
-//     error: function (data) {
-//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//       console.error('chatterbox: Failed to clear messages', data);
-//     }
-//   })
-// };
-//
-// MakeApp.prototype.renderMessage = function (message) {
-//   $.ajax({
-//     url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-//     type: 'GET',
-//     data: JSON.stringify(message),
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log('chatterbox: Message fetched');
-//     },
-//     error: function (data) {
-//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//       console.error('chatterbox: Failed to fetch message', data);
-//     }
-//   })
-// };
-//
-// MakeApp.prototype.renderRoom = function (roomName) {
-//   // $.ajax({
-//   //   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-//   //   type: 'GET',
-//   //   data: JSON.stringify(message),
-//   //   contentType: 'application/json',
-//   //   success: function (data) {
-//   //     console.log('chatterbox: Message fetched');
-//   //   },
-//   //   error: function (data) {
-//   //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//   //     console.error('chatterbox: Failed to fetch message', data);
-//   //   }
-//   // })
-// };
-//
-// let app = new MakeApp();
+
+  class MakeApp {
+
+    constructor() {}
+
+    send (message) {
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message sent');
+        },
+        error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message', data);
+        }
+      })
+    }
+
+    fetch() {
+      $.ajax({
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message fetched');
+        },
+        error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to fetch message', data);
+        }
+      })
+    }
+
+    init() {
+      // $(document).ready(function() {});
+    }
+
+    clearMessages() {
+      $('div').remove('#chats');
+      $('#main').append('<div id="chats"></div>');
+    }
+
+
+    renderMessage(message) {
+      var userClass = message.username.split(' ').join('');
+      $('#chats').prepend('<p class"' + userClass + ' ' + message.roomname + '"><b>' + message.username + ': </b>' + message.text + '</p>');
+    }
+
+    renderRoom(roomName) {
+      // $.ajax({
+      //   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      //   type: 'GET',
+      //   data: JSON.stringify(message),
+      //   contentType: 'application/json',
+      //   success: function (data) {
+      //     console.log('chatterbox: Message fetched');
+      //   },
+      //   error: function (data) {
+      //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      //     console.error('chatterbox: Failed to fetch message', data);
+      //   }
+      // })
+    }
+
+  }
+
+  var app = new MakeApp();
