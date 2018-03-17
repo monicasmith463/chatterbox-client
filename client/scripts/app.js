57889ef0a91e6688a68ace86469226dbdf331a0a
monicasmith463 <monica.smith463@gmail.com>
2018-03-17 12:31:20 -0700

    Third commit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
 
-var $body = $('body');
-$body.append('<div>hello</div>');
-$body.append(app);
-var $header = $('<h1>Twittler</h1></br><h3>What\'s on your mind?</h3>');
-$body.append($header);
+// var $body = $('body');
+// $body.append('<div>hello</div>');
+// $body.append(app);
+// var $header = $('<h1>Twittler</h1></br><h3>What\'s on your mind?</h3>');
+// $body.append($header);
 
 
   class MakeApp {
 
     constructor() {
-      this.posts = [];
+      this.posts = [
+        {
+          username: 'Mel Brooks',
+          text: 'It\'s good to be the king',
+          roomname: 'lobby'
+        }
+      ];
+      this.users = [];
+      this.rooms = ['main', 'random', 'help'];
       // this.form = //…
       // this.friends = //…
       
@@ -39,6 +47,7 @@ $body.append($header);
 
     fetch() {
       $.ajax({
+        url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
         type: 'GET',
         contentType: 'application/json',
         success: function (data) {
@@ -53,8 +62,32 @@ $body.append($header);
 
     init() {
       // $(document).ready(function() {});
+      
+      // render dropdown
+      /// retrieve room names
+      /// push to rooms array, maybe
+      // var renderRoom = this.renderRoom.bind(this);
+      debugger;
+      console.log(this.fetch());
+      // this.rooms.forEach( function (room) {
+      //   this.renderRoom(room);
+      //   // room = '<option value = "' + room + '">' + room + '</option>';
+      //   // $('#roomSelect').append(room);
+      // })
+      
+      // renderMessage()
+
+      /// room select
+      // call first functions
+      // renderRoom(/*default*/);
+      // var handleSubmit = this.handleSubmit.bind(this);
+      // var handleClick = this.handleSubmit.submit(handleSubmit())
       // bind function to select block
-      $('#send .submit').bind(this.handleSubmit());
+      let app = this;
+      $('#send').on("click", function() {app.handleSubmit()} );
+      // $('#send').submit((this).handleSubmit());
+      // $('#send .submit').submit(this.handleSubmit());
+
     }
 
     clearMessages() {
@@ -95,11 +128,12 @@ $body.append($header);
     }
 
     handleSubmit() {
-      $(this).submit(function(event) {
-        // var context = $(this);
-        // alert( "Handler for .submit() called." );
-        // event.preventDefault();
-      })
+      console.log(this);
+      
+
+      // var context = $(this);
+      alert( "Handler for .submit called." );
+      // event.preventDefault();
     }
 
     addFriend() {
